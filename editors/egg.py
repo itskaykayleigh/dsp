f4487cd707050553d25fca51332cd202588342a6
itskaykayleigh <liyuqiao1@gmail.com>
2017-08-30 12:38:41 -0400

    Add files via upload

---
+++ editors/egg.py
@@ -0,0 +1,3 @@
+print("Egg is one of my favorite food")
+print("And I even have a frenchie named Eggy")
+print("Isn't his name super cute?!") 
---
+++ editors/vinegar.py
@@ -0,0 +1,3 @@
+print("I do not like vinegar that much")
+print("But I still use it sometimes for my noodles")
+
