c4ea5f66ceb39f80e602a5ebebc8cd6d25367b72
itskaykayleigh <liyuqiao1@gmail.com>
2017-09-08 22:12:02 -0500

    Update advanced_python_regex.py

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -43,3 +43,36 @@ with open('faculty.csv', 'r') as csvfile:
 		emails = []
 		emails.append(line[3])
 		print emails
+
+# Q4. 
+# Find how many different email domains there are 
+# (Ex:  mail.med.upenn.edu, upenn.edu, email.chop.edu, etc.).  
+# Print the list of unique email domains.
+
+import csv
+
+def process_file(filename):
+	hist = {}
+	with open('faculty.csv', 'r') as csvfile:
+		facultyreader = csv.reader(csvfile)
+# with open('degrees.csv', 'w') as newfile:
+# 	degrees_writer = csv.writer(newfile, delimiter='\t')
+		next(facultyreader)  			
+
+		for line in facultyreader:
+			name, domain = line[3].split('@')
+			process_line(domain, hist)
+		return hist 
+
+def process_line(domain, hist):
+			hist[domain] = hist.get(domain, 0) + 1 
+
+hist = process_file('faculty.csv')		
+
+def different_domain(hist):
+	return len(hist)
+print 'Number of different domain:', different_domain(hist)	
+print hist 
+
+# Number of different domain: 4
+# {'email.chop.edu': 1, 'upenn.edu': 12, 'cceb.med.upenn.edu': 1, 'mail.med.upenn.edu': 23}
